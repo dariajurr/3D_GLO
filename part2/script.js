2ea1ca1a46859ae20f27fb1cfd00acfecd8c4787
dariajurr <63465787+dariajurr@users.noreply.github.com>
2020-09-02 15:37:31 +0700

    feat: homework18 | script.js, part2

--- part2/script.js
+++ part2/script.js
@@ -1,59 +1,67 @@
-/* Вывести текущий день и время  на страницу в таком формате
 
-Добрый день (утро, вечер, ночь в зависимости от времени суток)
-Сегодня: Понедельник
-Текущее время:12:05:15 PM
-До нового года осталось 175 дней */
 'use strict';
+const date2 = new Date();
+let year = date2.getFullYear()+1;
 
-
-function countTimer(deadline) {
-  const date = new Date();
-
-  function getTimeRemaining() {
-
+function countTimer(deadline) {    
+  const item = document.createElement('pre');  
+  document.body.append(item);
+  
+  function getTimeRemaining() {    
     const dateStop = new Date(deadline).getTime();
-    const dateNow = date.getTime();
+    const dateNow = new Date().getTime();
     const timeRemaining = (dateStop - dateNow) / 1000;
     const days = Math.floor(timeRemaining / 60 / 60 / 24);
-    return {
+    return {     
       timeRemaining,
       days,
     };
   }
 
-  function getTimeOfDay() {
-    const greeting = ['Доброй ночи', 'Доброе утро', 'Добрый день', 'Добрый вечер'];
-    const hours = date.getHours();
+  function getTextForm(n) {
+    const days = ["дней", "день", "дня"];
+    const n1 = n % 100;
+    n %= 10;
+    return days[
+      n1 > 10 && n1 < 20 ? 0 : n === 1 ? 1 : n > 1 && n < 5 ? 2 : 0
+    ];
+  };
+  
+  function getTimeOfDay(hours) {
+    const greeting = ['Доброй ночи', 'Доброе утро', 'Добрый день', 'Добрый вечер'];    
     return greeting[
-      hours < 6 ? 0 : hours < 12 ? 1 : hours < 12 ? 2 : 3
+      hours < 6 ? 0 : hours < 12 ? 1 : hours < 18 ? 2 : 3
     ];
   }
 
-  function getWeekDay() {
+  function getWeekDay(day) {
     const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-    return days[date.getDay()];
+    return days[day];
   }
 
-  function updateClock() {
+  
+  
+  function updateClock() {    
     const timer = getTimeRemaining();
+    const date = new Date();
+      
 
-
-    document.body.textContent = `${getTimeOfDay()} 
-    Сегодня: ${getWeekDay()}
+    item.textContent = (`
+    ${getTimeOfDay(date.getHours())}
+    Сегодня: ${getWeekDay(date.getDay())}
     Текущее время: ${date.toLocaleTimeString("en-US")}
-    До нового года осталось ${timer.days} дней`;
-
-
-
-    if (timer.timeRemaining > 0) {
+    До нового года осталось ${timer.days} ${getTextForm(timer.days)}`);
+    
+    
+    if (timer.timeRemaining >= 0) {      
       setTimeout(updateClock, 1000);
+    } else {
+      setTimeout(updateClock, 1000);
+      year+=1;
+      countTimer(`31 december ${year}`);
     }
 
-  }
+  };
   updateClock();
-
-
-
 }
-countTimer('31 december 2020');
+countTimer(`1 january ${year}`);
